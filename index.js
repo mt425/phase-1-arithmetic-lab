c4f2b3b4f358cdedf127f4e1823cd3a81067b7e5
Matthew <mtgreff5@gmail.com>
2022-10-17 19:57:00 -0400

    commit tree 5aee333a2dc7af15104ad160e2df8a3cd760d7f8

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply;
+let num1 = 31
+let num2 = 2
+multiply = num1 * num2;
 
-// Write your code here
+let random;
+random = Math.ceil(Math.random());
+
+let mod;
+let num3 = 16;
+let num4 = 6;
+mod = num3 % num4;
+
+let max;
+max = Math.max(5, 10, 15, 20);
